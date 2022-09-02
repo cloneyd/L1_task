8fa09c30d84867baea78020a0fe2ccfa1aeca8e7
cloneyd <62465708+cloneyd@users.noreply.github.com>
2022-09-02 18:03:32 +0300

    done 25 task

--- 25_my_sleep/main.go
+++ 25_my_sleep/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+func sleep(dur time.Duration) {
+	end := time.Now().Add(dur)
+
+	for time.Now().Before(end) {
+		continue
+	}
+}
 
 func main() {
-	time.Sleep()
+	start := time.Now()
+	dur := time.Duration(500 * time.Millisecond)
+
+	sleep(dur)
+
+	fmt.Println("Elapsed", time.Since(start))
 }
